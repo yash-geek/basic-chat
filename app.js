44cc88ad4699efb718b8f184ac530ea351712aa8
yash-geek <yashgautam8738@gmail.com>
2025-07-13 19:47:43 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,41 @@
+import { count } from 'console';
+import express from 'express'
+import http from 'http'
+import path from 'path'
+import { Server } from 'socket.io';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = 3000;
+
+
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+//handle all socket requests
+io.on('connection',(socket)=>{
+    console.log('new User', socket.id)
+    socket.on('user-message',message=>{
+        io.emit('message',message);
+    })
+})
+
+
+
+
+//express will handle all http requests
+app.use(express.json());
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.get('/',(req,res,next)=>{
+    res.sendFile('/public/index.html')
+})
+
+server.listen(PORT,()=>{
+    console.log(`the app is running on http://localhost:${PORT}`);
+})
